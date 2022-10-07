7471c975a7215b4ad7ba6375f90e25a501a69657
Belu <belulop_@MacBook-Pro-de-Belu.local>
2022-10-07 01:52:41 -0300

    dashboard en progreso

---
+++ src/components/TopBar/TopBar.js
@@ -0,0 +1,16 @@
+
+import './TopBar.css';
+
+
+
+function App() {
+  return (
+    <div  className='top-bar'>
+          <h1>
+            Bienvenidos al Dashboard de Iconic
+          </h1>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App/App';
+import TopBar from './components/TopBar/TopBar'
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <TopBar/>
     <App />
+  
   </React.StrictMode>
 );
 
