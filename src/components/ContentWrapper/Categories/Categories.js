04bfa8a7482ce832e2a326b5285e18048af68ea4
Belu <belulop_@MacBook-Pro-de-Belu.local>
2022-10-13 20:24:29 -0300

    apis en proceso

--- src/components/ContentWrapper/Categories/Categories.js
+++ src/components/ContentWrapper/Categories/Categories.js
@@ -1,80 +1,49 @@
-
+import React, {Component} from "react";
+import CategoriesCard from "../CategoriesCard/CategoriesCard";
 import './Categories.css';
 
+class Categories extends Component {
+    constructor() {
+        super()
+        this.state = {
+            categoriesList: []
+        }
+    }
+    componentDidMount(){
+        let categoriesList= []
+        fetch('http://localhost:3001/products/api/products')
+        .then(result =>{
+            return result.json()
+        })
+        .then(categories=>{
+            //  this.setState({categoriesList : [...categories.countByCategory]})
+            categoriesList.push(categories)
+            console.log(categories.countByCategory)
 
+        })
+        .catch(error=>{
+            console.log(error)
+        })
+    }
 
-function Categories(props) {
-  return (
-    <div  className='categories'>
-     
-        <div className="categories-content">
-               
-                                    <div className="category-card">
-                                       
-                                            <div className="category-name">
-                                                <div className="name">Casual Clothing</div>
-                                               
-                                            </div>
-                                            <div className="category-count">
-                                                10
-                                            </div>
-                                       
-                                    </div>
-                             
-        </div>
-        <div className="categories-content">
-               
-               <div className="category-card">
-                  
-                       <div className="category-name">
-                           <div className="name">Casual Clothing</div>
-                          
-                       </div>
-                       <div className="category-count">
-                           32
-                       </div>
-                  
-               </div>
-        
-    </div>
-    <div className="categories-content">
-               
-               <div className="category-card">
-                  
-                       <div className="category-name">
-                           <div className="name">Casual Clothing</div>
-                          
-                       </div>
-                       <div className="category-count">
-                           9
-                       </div>
-                  
-               </div>
-
-        
-    </div>
-    <div className="categories-content">
-               
-               <div className="category-card">
-                  
-                       <div className="category-name">
-                           <div className="name">Casual Clothing</div>
-                          
-                       </div>
-                       <div className="category-count">
-                         67
-                       </div>
-                  
-               </div>
-        
-</div>
-
-
-
-
-    </div>
-    
-  );
+ 
+    render(){
+        return(
+            <React.Fragment>
+            <div className="title">
+                <h1>Categorías en la base de datos</h1>
+            </div>
+            <div className="row">
+             {   
+             this.state.categoriesList.map((category,index)=>{
+                return <CategoriesCard {...category} key = {index}/>
+             })
+           
+             }
+            </div>
+       </React.Fragment>
+        )
+       
+    }
 }
-
 export default Categories;
---
+++ src/components/ContentWrapper/CategoriesCard/CategoriesCard.js
@@ -0,0 +1,33 @@
+
+import React from 'react'
+import './CategoriesCard.css';
+
+
+
+function CategoriesCard(props) {
+  return (
+    <div  className='categories'>
+     
+        <div className="categories-content">
+               
+                                    <div className="category-card">
+                                       
+                                            <div className="category-name">
+                                                <div className="name">{props.name}</div>
+                                               
+                                            </div>
+                                            <div className="category-count">
+                                                {props.count}
+                                            </div>
+                                       
+                                    </div>
+                             
+        </div>
+    
+
+    </div>
+    
+  );
+}
+
+export default CategoriesCard;
