2993cf074a3d882f4c09a6703172bd29ad4c37dd
hollerller <hollerller@gmail.com>
2022-10-13 00:59:52 -0500

    LLamado de APIs de usuarios y productos

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,51 +4,57 @@ import TopBar from '../ContentWrapper/TopBar/TopBar'
 import Content from '../ContentWrapper/Content/Content'
 import LastProduct from '../ContentWrapper/LastProduct/LastProduct';
 import Categories from '../ContentWrapper/Categories/Categories';
+
+
 function App() {
 
-
   let info = [
-      {
-        title: "Total de Productos",
-        count: 105,
-        icon: "flag"
-      },
-      {
-        title:"Total de Usuarios",
-        count:4,
-        icon:"users"
-      },
-      {
-        title: "Total de Categorías",
-        count: 4,
-        icon: "heart"
-      }
+    {
+      title: "Total de Productos",
+      count: 105,
+      icon: "flag"
+    },
+    {
+      title: "Total de Usuarios",
+      count: 4,
+      icon: "users"
+    },
+    {
+      title: "Total de Categorías",
+      count: 4,
+      icon: "heart"
+    }
   ]
 
   return (
 
     <div className='content-father'>
-        <div className='top-bar'>
-          <TopBar/>
-        </div>
-         <div className='content-row'>
-          {/* <Content/>
+      <div className='top-bar'>
+        <TopBar />
+      </div>
+      <div className='content-row'>
+        {/* <Content/>
           <Content/>
           <Content/> */}
+        
+        
 
           {
             info.map((item,i)=>{
              return  <Content title={item.title} count={item.count} icon={item.icon} key={i}/>
             })
           }
-         </div>
-        <div className='last-product'>
-          <LastProduct/>
-        </div>
-        <div className='categories-count'>
-          <Categories/>
-        </div>
-          
+
+        {/* <Content /> */}
+
+      </div>
+      <div className='last-product'>
+        <LastProduct />
+      </div>
+      <div className='categories-count'>
+        <Categories />
+      </div>
+
     </div>
   );
 }
